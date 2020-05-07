509757f97c75d546e7769577c917daf7ad3c56e1
ueken65 <ueken6522@gmail.com>
2020-05-07 23:06:43 +0900

    test: intersectionObserver

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,7 @@ import Layout from '../../components/layout'
 import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import utilStyles from '../../styles/utils.module.css'
+import { useEffect, useState } from 'react'
 
 export const getStaticPaths = async () => {
   const paths = getAllPostIds()
@@ -22,12 +23,29 @@ export const getStaticProps = async ({ params }) => {
 }
 
 const Post = ({ postData }) => {
+  const [isInView, setIsInView] = useState(false)
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver(handleIntersectionObserver, {
+      threshold: [0, 1.0]
+    })
+    intersectionObserver.observe(document.getElementById('article'))
+  }, [])
+
+  const handleIntersectionObserver = (entries, _) => {
+    entries.forEach((entry) => {
+      setIsInView(true)
+      console.log(entry)
+      console.log('読了')
+    })
+  }
+
   return (
     <Layout>
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <article>
+      <article id='article' style={{background: isInView ? 'yellow' : 'white'}}>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
